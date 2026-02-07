d9791f82fda94564f935917359a2ef9f47c27ac9
bheowolfe <83993140+bheowolfe@users.noreply.github.com>
2026-02-07 21:47:30 +0000

    q3 q4 q5

--- README.md
+++ README.md
@@ -16,5 +16,44 @@
 
 ## Question 3
 
+    ny_taxi=# SELECT COUNT(*) FROM tripdata WHERE lpep_pickup_datetime >= '2025-11-01' AND lpep_pickup_datetime < '2025-12-01' AND trip_distance <= 1;
+
+        count 
+        -------
+        8007
+
+## Question 4
+
+    ny_taxi=# SELECT 
+        DATE(lpep_pickup_datetime),
+        MAX(trip_distance) as longest_trip
+    FROM tripdata
+    WHERE trip_distance < 100
+    GROUP BY DATE(lpep_pickup_datetime)
+    ORDER BY longest_trip DESC
+    LIMIT 1;
+
+            date    | longest_trip 
+        ------------+--------------
+        2025-11-14 |        88.03
+
+## Question 5
+
+    ny_taxi=# SELECT 
+        taxi_zone."Zone",
+        SUM(tripdata.total_amount) as total_amount
+    FROM tripdata 
+    JOIN taxi_zone ON tripdata."PULocationID" = taxi_zone."LocationID"
+    WHERE DATE(tripdata.lpep_pickup_datetime) = '2025-11-18'
+    GROUP BY taxi_zone."Zone"
+    ORDER BY total_amount DESC
+
+        LIMIT 1;
+            Zone        |   total_amount    
+        -------------------+-------------------
+        East Harlem North | 9281.919999999991
+
+## Question 6
+
     
 
--- SQL_Folder/lookup.py
+++ SQL_Folder/lookup.py
@@ -3,48 +3,16 @@ from sqlalchemy import create_engine
 from tqdm.auto import tqdm
 import click
 
-dtype = {
-    "VendorID": "Int64",
-    "passenger_count": "Int64",
-    "trip_distance": "float64",
-    "RatecodeID": "Int64",
-    "store_and_fwd_flag": "string",
-    "PULocationID": "Int64",
-    "DOLocationID": "Int64",
-    "payment_type": "Int64",
-    "fare_amount": "float64",
-    "extra": "float64",
-    "mta_tax": "float64",
-    "tip_amount": "float64",
-    "tolls_amount": "float64",
-    "improvement_surcharge": "float64",
-    "total_amount": "float64",
-    "congestion_surcharge": "float64"
-}
 
-parse_dates = [
-    "tpep_pickup_datetime",
-    "tpep_dropoff_datetime"
-]
-
-
-def ingest_data(
-        url: str,
+def ingest_csv_data(
         engine,
         target_table: str,
-        chunksize: int = 100000,
 ) -> pd.DataFrame:
-    df_iter = pd.read_csv(
-        url,
-        dtype=dtype,
-        parse_dates=parse_dates,
-        iterator=True,
-        chunksize=chunksize
+    df = pd.read_csv(
+        "/data/taxi_zone_lookup.csv",
     )
 
-    first_chunk = next(df_iter)
-
-    first_chunk.head(0).to_sql(
+    df.to_sql(
         name=target_table,
         con=engine,
         if_exists="replace"
@@ -52,23 +20,23 @@ def ingest_data(
 
     print(f"Table {target_table} created")
 
-    first_chunk.to_sql(
-        name=target_table,
-        con=engine,
-        if_exists="append"
+
+def ingest_parquet_data(
+        engine,
+        target_table: str,
+) -> pd.DataFrame:
+    df = pd.read_parquet(
+        "/data/green_tripdata_2025-11.parquet"
     )
 
-    print(f"Inserted first chunk: {len(first_chunk)}")
+    df.to_sql(
+        name=target_table,
+        con=engine,
+        if_exists="replace"
+    )
 
-    for df_chunk in tqdm(df_iter):
-        df_chunk.to_sql(
-            name=target_table,
-            con=engine,
-            if_exists="append"
-        )
-        print(f"Inserted chunk: {len(df_chunk)}")
+    print(f"Table {target_table} created")
 
-    print(f'done ingesting to {target_table}')
 
 @click.command()
 @click.option('--pg-user', default='root', show_default=True, help='Postgres user')
@@ -76,21 +44,19 @@ def ingest_data(
 @click.option('--pg-host', default='localhost', show_default=True, help='Postgres host')
 @click.option('--pg-port', default=5432, show_default=True, type=int, help='Postgres port')
 @click.option('--pg-db', default='ny_taxi', show_default=True, help='Postgres database')
-@click.option('--year', default=2021, show_default=True, type=int, help='Data year')
-@click.option('--month', default=1, show_default=True, type=int, help='Data month')
-@click.option('--chunksize', default=100000, show_default=True, type=int, help='CSV read chunksize')
-@click.option('--target-table', default='yellow_taxi_data', show_default=True, help='Target DB table')
-@click.option('--url-prefix', default='https://github.com/DataTalksClub/nyc-tlc-data/releases/download/yellow', show_default=True, help='URL prefix for data files')
-def main(pg_user, pg_pass, pg_host, pg_port, pg_db, year, month, chunksize, target_table, url_prefix):
+@click.option('--target-csv-table', default='taxi_zone', show_default=True, help='Target DB table')
+@click.option('--target-parquet-table', default='tripdata', show_default=True, help='Target DB table')
+def main(pg_user, pg_pass, pg_host, pg_port, pg_db, target_csv_table, target_parquet_table):
     engine = create_engine(f'postgresql://{pg_user}:{pg_pass}@{pg_host}:{pg_port}/{pg_db}')
 
-    url = f'{url_prefix}/yellow_tripdata_{year:04d}-{month:02d}.csv.gz'
-
-    ingest_data(
-        url=url,
+    ingest_csv_data(
         engine=engine,
-        target_table=target_table,
-        chunksize=chunksize,
+        target_table=target_csv_table,
+    )
+
+    ingest_parquet_data(
+        engine=engine,
+        target_table=target_parquet_table,
     )
 
 if __name__ == '__main__':
